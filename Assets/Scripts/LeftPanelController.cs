b6806dded51846bfd1e61fcdab60c39f46411fec
Ruben Navarro Davo <ruben.navarrodavo@gmail.com>
2020-08-28 20:18:12 +0200

    imp: Se ha añadido el control para saber si se ha dado derecha o izquierda. Se detecta cuando lo pulsa y cuando lo suelta.

---
+++ Assets/Scripts/LeftPanelController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class LeftPanelController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+    public bool buttonPressed;
+
+    public void OnPointerDown(PointerEventData eventData) {
+        buttonPressed = true;
+        Debug.Log(" <--  Mover izquierda.");
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        buttonPressed = false;
+        Debug.Log("|<-- Parar movimiento izquierda.");
+    }
+}
---
+++ Assets/Scripts/RightPanelController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class RightPanelController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+        public bool buttonPressed;
+
+        public void OnPointerDown(PointerEventData eventData) {
+            buttonPressed = true;
+        Debug.Log(" -->  Mover derecha.");
+        }
+
+        public void OnPointerUp(PointerEventData eventData) {
+            buttonPressed = false;
+        Debug.Log(" -->| Parar movimiento derecha.");
+    }
+    }
