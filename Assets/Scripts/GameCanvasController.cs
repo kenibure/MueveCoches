fe5a7b801de5feaf503c2cb159d32e8a54b60472
Ruben Navarro Davo <ruben.navarrodavo@gmail.com>
2020-09-05 20:27:44 +0200

    imp:Se añade el movimiento del suelo.

--- Assets/Scripts/GameCanvasController.cs
+++ Assets/Scripts/GameCanvasController.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GameCanvasController : MonoBehaviour
-{
+public class GameCanvasController : MonoBehaviour {
+
+    private float parallaxSpeed = 0.1f;
+    public RawImage background; //Desde Unity hay que asignarle cual es.
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +18,19 @@ public class GameCanvasController : MonoBehaviour
     // Update is called once per frame
     void Update() {
 
-        /*if(Input.GetMouseButton(0)) {
-            Debug.Log("Se esta pulsando un boton");
-        }*/
+        efectoParallax();
+    }
+    private void efectoParallax() { //Es el efecto de que el fondo se mueva más lento que la plataforma para dar sensación de produndidad.
+        float finalSpeed = parallaxSpeed * Time.deltaTime; //Debe calcularse para que se adecúe a la pantalla, por eso no puede usarse directamente parallaxSpeed.
+        moverFondo(finalSpeed);
+        //moverPlataforma(finalSpeed);
+    }
+
+    private void moverFondo(float speed) {
+        Rect newRect = new Rect(background.uvRect.x, background.uvRect.y + speed, background.uvRect.width, background.uvRect.height);
+        moverRawImage(background, newRect);
+    }
+    private void moverRawImage(RawImage rawImage, Rect rect) {
+        rawImage.uvRect = rect;
     }
 }
